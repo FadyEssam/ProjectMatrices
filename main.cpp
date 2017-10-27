ed962f5ad2b8068641ce0fc089eeccac039d64fe
FadyEssam <fadyesam1996@gmail.com>
2017-10-28 00:30:18 +0200

    added file hanling from args and make file editing

--- main.cpp
+++ main.cpp
@@ -3,19 +3,29 @@
 #include "Parser.h"
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
-	Parser parser;
-	 string path = "/home/fady/matrices/commands";
-	 parser.load(path);
-	
 
-	// string s;
-	// while (1)
-	// {
-	// 	getline(cin, s);
-	// 	if (!s.compare("exit")) break;
-	// 	parser.handle(s);
-	// }
+	Parser parser;
+
+	if(argc>1)
+
+		{
+			 parser.load(argv[1]);
+		}
+
+	else
+
+		{
+			string s;
+			while (1)
+			{
+				getline(cin, s);
+				if (!s.compare("exit")) break;
+				parser.handle(s);
+			}
+
+		}
+
 	return 0;
 }
--- makefile
+++ makefile
@@ -2,6 +2,10 @@ CC=g++
 Deps= main.cpp Matrix.cpp Parser.cpp
 OutputName=output.exe
 
+test: $(Deps)
+	$(CC) $(Deps) -o $(OutputName)
+	./$(OutputName)
+	
 build: $(Deps)
 	$(CC) $(Deps) -o $(OutputName)
 
